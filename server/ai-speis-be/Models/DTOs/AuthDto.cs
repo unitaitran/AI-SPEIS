770ec64c0a5f98e6e299254df537739eb03679b4
dvquyen2609 <chinieko0@gmail.com>
2026-06-14 09:06:14 +0700

    modify authenticaton dto

--- server/ai-speis-be/Models/DTOs/AuthDto.cs
+++ server/ai-speis-be/Models/DTOs/AuthDto.cs
@@ -5,9 +5,10 @@ namespace ai_speis_be.Models.DTOs
     public class LoginDto
     {
         [Required]
-        [EmailAddress]
+        [EmailAddress(ErrorMessage = "Địa chỉ email không hợp lệ.")]
         public string Email { get; set; } = null!;
         [Required]
+        [MinLength(6, ErrorMessage = "Mật khẩu phải ít nhất 6 ký tự.")]
         public string Password { get; set; } = null!;
     }
     public class LoginResponseDto
@@ -21,14 +22,16 @@ namespace ai_speis_be.Models.DTOs
 
     public class RegisterDto
     {
-        [Required]
+        [Required(ErrorMessage = "Họ và tên là bắt buộc.")]
         public string FullName { get; set; } = null!;
         [Required]
-        [EmailAddress]
+        [EmailAddress(ErrorMessage = "Địa chỉ email không hợp lệ.")]
         public string Email { get; set; } = null!;
-        [Phone]
+        [MaxLength(32)]
+        [RegularExpression(@"^0\d{9}$", ErrorMessage = "Số điện thoại phải có 10 chữ số và bắt đầu bằng 0")]
         public string? PhoneNumber { get; set; }
         [Required]
+        [MinLength(6, ErrorMessage = "Mật khẩu phải ít nhất 6 ký tự.")]
         public string Password { get; set; } = null!;
     }
 
