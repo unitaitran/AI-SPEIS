b57d06bb6e85eed989bd073fc60dec26bce6a460
dvquyen2609 <chinieko0@gmail.com>
2026-07-12 17:27:48 +0700

    fix soft delete in interviewsession

---
+++ server/ai-speis-be/Migrations/20260712102700_SoftDeleteSession.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ai_speis_be.Migrations
+{
+    /// <inheritdoc />
+    public partial class SoftDeleteSession : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "InterviewSession",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "InterviewSession",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "InterviewCampaign",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "InterviewCampaign",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "InterviewSession");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "InterviewSession");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "InterviewCampaign");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "InterviewCampaign");
+        }
+    }
+}
